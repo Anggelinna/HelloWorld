71a06ceec124654d41f41b62cf1ec6f6974c0ca5
Anggelinna <lina.koloytanova.03@yandex.ru>
2023-09-09 22:47:28 +0500

    выполнила задания 4-9

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let a = 20;
+let s = 10;
+let s1 = 20;
 // a = prompt('Сколько вам лет?');
 
-alert(a);
+alert(s);
+alert(s1);
 
 
 let year = 2007;
@@ -10,3 +12,46 @@ let year = 2007;
 alert(year);
 
 
+let userName = 'Brendan';
+// userName = prompt('Кто создал JavaScript?');
+
+alert(userName);
+
+
+let a1 = 10;
+let a2 = 2;
+alert(a1 + a2);  // будет 12
+
+alert(a1 - a2);  // будет 8
+
+alert(a1 / a2);  // будет 5
+
+
+let result = 2;
+alert(2 ** 5);  // будет 32
+
+
+let a = 9;
+let b = 2;
+alert(a % b); // будет 1
+
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);  // будет 7
+
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+
+const user = {
+    name: 'Mira',
+    age: 22,
+    isAdmin: false,
+}
