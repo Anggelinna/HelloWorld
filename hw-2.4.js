0e7fc2808ea90f476f246e534faf226650d66e7b
Anggelinna <lina.koloytanova.03@yandex.ru>
2023-09-27 18:15:48 +0500

    задание 6

--- hw-2.4.js
+++ hw-2.4.js
@@ -36,67 +36,7 @@ while (n > 50) {
 }   
 
 
-let day = prompt('Введите число от 1 до 31');   // задание 6
-if (day == 1) {
-alert('Понедельник');
-} else if (day == 2) {
-alert('Вторнк');
-} else if (day == 3) {
-alert('Среда');
-} else if (day == 4) {
-alert('Четверг');
-} else if (day == 5) {
-alert('Сегодня пятница, 5 число. Необходимо подготовить отчет');
-} else if (day == 6) {
-alert('Суббота');
-} else if (day == 7) {
-alert('Воскресенье');
-} else if (day == 8) {
-alert('Понедельник');
-} else if (day == 9) {
-alert('Вторнк');
-} else if (day == 10) {
-alert('Среда');
-} else if (day == 11) {
-alert('Четверг');
-} else if (day == 12) {
-alert('Сегодня пятница, 12 число. Необходимо подготовить отчет');
-} else if (day == 13) {
-alert('Суббота');
-} else if (day == 14) {
-alert('Воскресенье');
-} else if (day == 15) {
-alert('Понедельник');
-} else if (day == 16) {
-alert('Вторнк');
-} else if (day == 17) {
-alert('Среда');
-} else if (day == 18) {
-alert('Четверг');
-} else if (day == 19) {
-alert('Сегодня пятница, 19 число. Необходимо подготовить отчет');
-} else if (day == 20) {
-alert('Суббота');
-} else if (day == 21) {
-alert('Воскресенье');
-} else if (day == 22) {
-alert('Понедельник');
-} else if (day == 23) {
-alert('Вторнк');
-} else if (day == 24) {
-alert('Среда');
-} else if (day == 25) {
-alert('Четверг');
-} else if (day == 26) {
-alert('Сегодня пятница, 26 число. Необходимо подготовить отчет');
-} else if (day == 27) {
-alert('Суббота');
-} else if (day == 28) {
-alert('Воскресенье');
-} else if (day == 29) {
-alert('Понедельник');
-} else if (day == 30) {
-alert('Вторнк');
-} else if (day == 31) {
-alert('Среда');
+let friday = 5;   // задание 6
+for(let i = friday; i <=31; i+=7){
+   console.log(`Сегодня пятница ${i}-е число.Необходимо подготовить отчет.`);
 }
