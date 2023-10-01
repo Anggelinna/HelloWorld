d4cd24b1ac066e32651e015aedd86e43a4fa33f2
Anggelinna <lina.koloytanova.03@yandex.ru>
2023-10-01 15:15:58 +0500

    задание 2, 3

--- hw-2.5.js
+++ hw-2.5.js
@@ -1,9 +1,23 @@
-min(4, 8) == 4;  // задание 1
-min(5, 6) == 5;
-function min(a, b) {
-    if (a < b) {
-      return a;
-    } else {
-      return b;
-    }
+function min(a, b) {   // задание 1
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
 }
+console.log(min(4, 8));
+console.log(min(5, 6));
+
+
+function even_or_odd(number) {    // задание 2
+  return number % 2 === 0 ? "Число четное" : "Число нечетное"
+}
+console.log(even_or_odd(2));
+console.log(even_or_odd(7));
+
+
+function number(num) {   // задание 3.1
+  return num * num
+}
+ 
+console.log(number(15));
