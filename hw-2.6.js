5524599684532121837e2dec397c693c0c31a9f9
Anggelinna <lina.koloytanova.03@yandex.ru>
2023-10-13 00:38:11 +0500

    задание 1-5

---
+++ hw-2.6.js
@@ -0,0 +1,32 @@
+let arr = [1, 5, 4, 10, 0, 3];   //задание 1
+
+for (let i = 0; i <= 3; i++) {
+	console.log(arr[i]);
+}
+
+
+let rrr = [1, 5, 4, 10, 0, 3];   //задание 2
+console.log(rrr.indexOf(4) + 1);
+
+
+let ar = [1, 3, 5, 10, 20];   //задание 3
+ar = ar.join(' ');
+console.log(ar);
+
+
+let brr = [    //задание 4
+	[1, 1, 1],
+	[1, 1, 1], 
+	[1, 1, 1],
+];
+console.log(brr[0]);
+console.log(brr[1]);
+console.log(brr[2])
+
+
+let der = [1, 1, 1];   //задание 5
+der.push(2, 2, 2);
+console.log(der);
+
+
+let crr = [9, 8, 7, 'a', 6, 5];
