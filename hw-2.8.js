e5fde12086591230537afbca9a4b8bb51e9976e7
Anggelinna <lina.koloytanova.03@yandex.ru>
2023-10-30 18:45:34 +0500

    задание 1

---
+++ hw-2.8.js
@@ -0,0 +1,16 @@
+const people = [   // задание 1
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ people.sort(function (a, b) {
+    if (a.age > b.age) {
+        return 1;
+    }
+    if (a.age < b.age) {
+        return -1;
+    }
+    return 0;
+ });
+console.log(people);
